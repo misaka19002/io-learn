d70cc22d1c8e4d5ae79a26a9a065ce83d2a17f17
wyd <wyudi@qq.com>
2019-02-12 15:30:37 +0800

    biopro httpserver 示例 使用线程池来执行任务

---
+++ src/main/java/com/rainday/biopro/BIOProServer.java
@@ -0,0 +1,52 @@
+package com.rainday.biopro;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by wyd on 2019/1/31 15:05:37.
+ * bio工作原理
+ */
+public class BIOProServer {
+    
+    private static final int DEFAULT_PORT = 800;
+    private static final int THREAD_SIZE = 3;
+    
+    public static void main(String[] args) {
+        AtomicLong count = new AtomicLong();
+        ServerSocket serverSocket = null;
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_SIZE);
+        try {
+            //创建serverSocket，并监听端口
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("服务器已启动，端口号：" + DEFAULT_PORT);
+            while (true) {
+                System.out.println("loop: " + count.getAndIncrement());
+                //当有链接建立。accept方法会阻塞执行，直到链接建立成功
+                Socket socket = serverSocket.accept();
+                //当有新的客户端接入时，会执行下面的代码
+                //使用现有的线程池执行提交的任务,下面三种写法均可
+                CompletableFuture.runAsync(new ServerHandler(socket), executorService);
+//                executorService.submit(new ServerHandler(socket));
+//                executorService.execute(new ServerHandler(socket));
+            }
+        } catch (IOException e) {
+        
+        } finally {
+            //一些必要的清理工作
+            if (serverSocket != null) {
+                System.out.println("即将关闭服务器。。。");
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/rainday/biopro/ServerHandler.java
@@ -0,0 +1,105 @@
+package com.rainday.biopro;
+
+import com.rainday.Calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * Created by wyd on 2019/1/31 19:15:02.
+ */
+public class ServerHandler implements Runnable {
+    
+    private Socket socket;
+    
+    public ServerHandler() {
+    }
+    
+    public ServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+    
+    @Override
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter writer = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.defaultCharset()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            String queryString = null;
+            String bodyString = null;
+            
+            /**
+             * 接受HTTP请求
+             */
+            String requestHeader;
+            int contentLength = 0;
+            while ((requestHeader = in.readLine()) != null && !requestHeader.isEmpty()) {
+                System.out.println("header:" + requestHeader);
+                /**
+                 * 获得GET参数
+                 */
+                if (requestHeader.startsWith("GET")) {
+                    int begin = requestHeader.indexOf("/?") + 2;
+                    int end = requestHeader.indexOf("HTTP/");
+                    queryString = requestHeader.substring(begin, end);
+                    System.out.println("GET参数是：" + queryString);
+                }
+                /**
+                 * 获得POST参数
+                 * 1.获取请求内容长度
+                 */
+                if (requestHeader.startsWith("Content-Length")) {
+                    int begin = requestHeader.indexOf("Content-Lengh:") + "Content-Length:".length();
+                    String postParamterLength = requestHeader.substring(begin).trim();
+                    contentLength = Integer.parseInt(postParamterLength);
+                    System.out.println("POST参数长度是：" + Integer.parseInt(postParamterLength));
+                }
+            }
+            StringBuffer sb = new StringBuffer();
+            if (contentLength > 0) {
+                for (int i = 0; i < contentLength; i++) {
+                    sb.append((char) in.read());
+                }
+                bodyString = sb.toString();
+                System.out.println("POST参数是：" + bodyString);
+            }
+            //发送回执
+            writer.println("HTTP/1.1 200 OK");
+            writer.println("Content-type:text/html");
+            writer.println();
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html lang=\"zh-CN\">");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\">");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h1>访问成功！:</h1>" + new Date());
+            Thread current = Thread.currentThread();
+            
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    
+            writer.println("<h1>thread:" + current.getName() + "  " + current.getId() + "</h1>");
+            writer.println("<h1>query:" + queryString + "</h1>");
+            writer.println("<h1>body:" + bodyString + "</h1>");
+            writer.println("<h1>result:" + Calculator.cal(queryString) + "</h1>");
+            writer.println("</body>");
+            writer.println("</html>");
+            
+            writer.flush();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
